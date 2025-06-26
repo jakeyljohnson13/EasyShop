bb1565d7f1b24db56867aff613a6ee523d042347
jakeyljohnson13 <jakeyl.johnson13@yahoo.com>
2025-06-26 14:48:47 -0700

    ShoppingCartDao and ShoppingCartController created.

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,13 +1,16 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
 import org.yearup.data.UserDao;
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 import org.yearup.models.User;
 
 import java.security.Principal;
@@ -15,17 +18,23 @@ import java.security.Principal;
 // convert this class to a REST controller
 @RestController
 @CrossOrigin
+@RequestMapping("cart")
 // only logged in users should have access to these actions
 public class ShoppingCartController
 {
     // a shopping cart requires
+    @Autowired
     private ShoppingCartDao shoppingCartDao;
+    @Autowired
     private UserDao userDao;
+    @Autowired
     private ProductDao productDao;
 
 
 
     // each method in this controller requires a Principal object as a parameter
+    @GetMapping
+    @PreAuthorize("isAuthenticated()")
     public ShoppingCart getCart(Principal principal)
     {
         try
@@ -35,9 +44,8 @@ public class ShoppingCartController
             // find database user by userId
             User user = userDao.getByUserName(userName);
             int userId = user.getId();
-
             // use the shoppingcartDao to get all items in the cart and return the cart
-            return null;
+            return shoppingCartDao.getByUserId(userId);
         }
         catch(Exception e)
         {
@@ -47,14 +55,74 @@ public class ShoppingCartController
 
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be added
+    @PostMapping("/products/{productId}")
+    @PreAuthorize("isAuthenticated()")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addToCart(@PathVariable int productId, Principal principal)
+    {
+        try
+        {
+            User user = userDao.getByUserName(principal.getName());
+            int userId = user.getId();
 
+            shoppingCartDao.addItem(userId,productId);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to add.");
+        }
+    }
 
     // add a PUT method to update an existing product in the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be updated)
     // the BODY should be a ShoppingCartItem - quantity is the only value that will be updated
+    @PutMapping("/products/{productId}")
+    @PreAuthorize("isAuthenticated()")
+    public void updateCartItem(
+            @PathVariable int productId,
+            @RequestBody ShoppingCartItem item,
+            Principal principal
+    ) {
+        try {
+            User user = userDao.getByUserName(principal.getName());
+            ShoppingCart cart = shoppingCartDao.getByUserId(user.getId());
 
+            if (!cart.contains(productId)){
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Item not found in cart.");
+            }
+            if (item.getProduct() == null){
+                item.setProduct(new Product());
+            }
+            item.getProduct().setProductId(productId);
+
+            shoppingCartDao.updateItem(user.getId(), item);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to update");
+        }
+    }
 
     // add a DELETE method to clear all products from the current users cart
     // https://localhost:8080/cart
-
+    @DeleteMapping
+    @PreAuthorize("isAuthenticated()")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCart(
+            @PathVariable int productId,
+            @RequestBody ShoppingCartItem item,
+            Principal principal
+    ){
+        try{
+            User user = userDao.getByUserName(principal.getName());
+            ShoppingCart cart = shoppingCartDao.getByUserId(user.getId());
+            if (cart.getItems().isEmpty()){
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Cart is already empty.");
+            }
+            shoppingCartDao.deleteCart(user.getId());
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- src/test/java/org/yearup/data/mysql/BaseDaoTestClass.java
+++ src/test/java/org/yearup/data/mysql/BaseDaoTestClass.java
@@ -1,30 +1,107 @@
 package org.yearup.data.mysql;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.yearup.configuration.TestDatabaseConfig;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MSSQLServerContainer;
+import org.testcontainers.utility.DockerImageName;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
 import java.sql.SQLException;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = TestDatabaseConfig.class)
-public abstract class BaseDaoTestClass
-{
+@SpringBootTest
+public abstract class BaseDaoTestClass {
+
+    // 1. Define the container. It will be a singleton for all tests that inherit this class.
+    static final MSSQLServerContainer<?> sqlServerContainer;
+
+    static {
+        // 2. Start the container.
+        sqlServerContainer = new MSSQLServerContainer<>(DockerImageName.parse("mcr.microsoft.com/mssql/server:2022-latest"))
+                .acceptLicense();
+        sqlServerContainer.start();
+    }
+
+    // 3. Dynamically set the datasource properties for the Spring context.
+    @DynamicPropertySource
+    static void setDatasourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", sqlServerContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", sqlServerContainer::getUsername);
+        registry.add("spring.datasource.password", sqlServerContainer::getPassword);
+        registry.add("spring.datasource.driver-class-name", sqlServerContainer::getDriverClassName);
+    }
+
     @Autowired
     protected DataSource dataSource;
 
-    @AfterEach
-    public void rollback() throws SQLException
-    {
-        Connection connection = dataSource.getConnection();
-        if(!connection.getAutoCommit())
-        {
-            dataSource.getConnection().rollback();
-        }
+    @BeforeEach
+    public void setupTestDatabase() throws SQLException {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        // A more robust way to clean the database
+        jdbcTemplate.execute("IF OBJECT_ID('shopping_cart', 'U') IS NOT NULL DROP TABLE shopping_cart;");
+        jdbcTemplate.execute("IF OBJECT_ID('order_line_items', 'U') IS NOT NULL DROP TABLE order_line_items;");
+        jdbcTemplate.execute("IF OBJECT_ID('orders', 'U') IS NOT NULL DROP TABLE orders;");
+        jdbcTemplate.execute("IF OBJECT_ID('products', 'U') IS NOT NULL DROP TABLE products;");
+        jdbcTemplate.execute("IF OBJECT_ID('categories', 'U') IS NOT NULL DROP TABLE categories;");
+        jdbcTemplate.execute("IF OBJECT_ID('profiles', 'U') IS NOT NULL DROP TABLE profiles;");
+        jdbcTemplate.execute("IF OBJECT_ID('users', 'U') IS NOT NULL DROP TABLE users;");
+
+        // Run the test-data script to repopulate the database
+        ScriptUtils.executeSqlScript(dataSource.getConnection(), new ClassPathResource("test-data.sql"));
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//package org.yearup.data.mysql;
+//
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//import org.yearup.configuration.TestDatabaseConfig;
+//
+//import javax.sql.DataSource;
+//import java.sql.Connection;
+//import java.sql.SQLException;
+//
+//@ExtendWith(SpringExtension.class)
+//@ContextConfiguration(classes = TestDatabaseConfig.class)
+//public abstract class BaseDaoTestClass
+//{
+//    @Autowired
+//    protected DataSource dataSource;
+//
+//    @AfterEach
+//    public void rollback() throws SQLException
+//    {
+//        Connection connection = dataSource.getConnection();
+//        if(!connection.getAutoCommit())
+//        {
+//            dataSource.getConnection().rollback();
+//        }
+//    }
+//}
